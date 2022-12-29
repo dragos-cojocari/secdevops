f2e20c99345dbcb01a179d88fa32bc944f65e779
Dragos Cojocari <dragos.cojocari@snyk.io>
2022-12-29 15:07:50 +0200

    update

---
+++ db.js
@@ -0,0 +1,6 @@
+
+var db_user=process.env.USER_ID;
+var db_pass="hj65_klhz/trlupok76";
+var db_name=process.env.DB_NAME;
+
+var mongo_uri = `mongodb+srv://${db_user}:${db_pass}@${db_name}.mongodb.net/test?retryWrites=true&w=majority`;
