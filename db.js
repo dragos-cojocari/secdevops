878dc964497b39ffd4e7afb32d2744353df5de95
Dragos Cojocari <dragos.cojocari@snyk.io>
2023-01-30 11:53:27 +0200

    update

--- db.js
+++
@@ -1,6 +0,0 @@
-
-var db_user=process.env.USER_ID;
-var api_supers="hj65_klhz/trlupok76";
-var db_name=process.env.DB_NAME;
-
-var mongo_uri = `mongodb+srv://${db_user}:${db_pass}@${db_name}.mongodb.net/test?retryWrites=true&w=majority`;
